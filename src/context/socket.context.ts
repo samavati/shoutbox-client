786756a0a9575b6a11a0d762f1b19ae53a5bff83
Ehsan Samavati <samaavaati@gmail.com>
2022-02-25 21:20:35 +0330

    feat(socket): base socket connection created

---
+++ src/context/socket.context.ts
@@ -0,0 +1,9 @@
+import { createContext, useContext } from "react";
+import socketio, { Socket } from "socket.io-client";
+
+export const socket = socketio(process.env.REACT_APP_SOCKET_URL as string);
+export const SocketContext = createContext<Socket | null>(null);
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+}
