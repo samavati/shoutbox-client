4b89b05e0d15b2043ba7d90afd6cf6e53e284578
Ehsan Samavati <samaavaati@gmail.com>
2022-02-25 17:02:48 +0330

    feat(message): message component created
    show users message text

---
+++ src/app/ShoutBox/components/Message/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import uniqolor from 'uniqolor';
+import { IMessage } from '../../../../model/Message';
+import { Typography, useTheme } from '@mui/material';
+
+interface MessageProps {
+    message: IMessage
+}
+
+const Message: React.FC<MessageProps> = ({ message }) => {
+
+    const attendeeName = message.isMe ? 'You' : message.attendee.name;
+    const theme = useTheme();
+
+    return (
+        <div style={{ display: 'flex', justifyContent: message.isMe ? 'end' : 'start' }}>
+            <Paper sx={{
+                maxWidth: '300px',
+                padding: '10px',
+                backgroundColor: message.isMe ? theme.palette.primary.main : undefined,
+                color: message.isMe ? 'white' : undefined
+            }}>
+                <Typography
+                    variant="caption"
+                    fontWeight="bold"
+                    style={{ color: message.isMe ? 'white' : uniqolor(attendeeName).color }}
+                >
+                    {attendeeName}
+                </Typography>
+                <Typography style={{ wordBreak: 'break-all' }}>{message.message}</Typography>
+            </Paper>
+        </div>
+    );
+}
+
+export default React.memo(Message);
