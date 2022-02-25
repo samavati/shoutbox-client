9501eb45c870c24f49bcd334465837c84d3735a8
Ehsan Samavati <samaavaati@gmail.com>
2022-02-25 17:03:45 +0330

    feat(attendee): attendees wrapper component

---
+++ src/app/ShoutBox/components/AttendeesWrapper/index.tsx
@@ -0,0 +1,8 @@
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+export const AttendeesWrapper = styled(Paper)(({ theme }) => ({
+    height: '100%',
+    width: '200px',
+    overflowY: 'auto'
+}))
