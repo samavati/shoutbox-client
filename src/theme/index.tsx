db00d82c0274878832a0aad66c7b47c19f21b322
Ehsan Samavati <samaavaati@gmail.com>
2022-02-25 17:11:05 +0330

    refactor: global theme setting for the MUI

---
+++ src/theme/index.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    shape: {
+        borderRadius: '8px'
+    },
+    palette: {
+        primary: {
+            main: '#ff6f61'
+        }
+    }
+});
