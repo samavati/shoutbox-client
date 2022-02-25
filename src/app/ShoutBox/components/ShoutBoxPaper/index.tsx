7fcaa17ced1e2a5b9efae4966839e23a6fcf8c5f
Ehsan Samavati <samaavaati@gmail.com>
2022-02-25 17:09:31 +0330

    feat(main): custom main page component style

---
+++ src/app/ShoutBox/components/ShoutBoxPaper/index.tsx
@@ -0,0 +1,9 @@
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+export const ShoutBoxPaper = styled(Paper)(({ theme }) => ({
+    backgroundColor: '#edf0f5',
+    height: '100%',
+    display: 'flex',
+    padding: '5px'
+}))
